eda755379ddd5a3473e07b70a585a4b3a64f2dbb
Ayaangautam <129714996+Ayaangautam@users.noreply.github.com>
2023-04-17 21:08:30 +0530

    Add files via upload

---
+++  Q2.py
@@ -0,0 +1,10 @@
+a=str("Ayaan")
+b=int("22107006")
+c=str("Mechanical")
+d=float(9.2)
+
+# We are using an F-string here so that we can directly use variables in the string
+
+print(f'''Hey, {a} here!
+My SID is {b}
+I am from {c} department and my CGPA is {d}''')
